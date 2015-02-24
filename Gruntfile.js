a5c6c41cfce4d14e8bfea568fdcfa00c00593cc0
VIDAL Julien <julien.vidal.jv@gmail.com>
2015-02-25 00:03:32 +0100

    Install first dependency and bootstrap project

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = function (grunt) {
+  // Show elapsed time at the end
+  require('time-grunt')(grunt);
+  // Load all grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      gruntfile: {
+        src: ['Gruntfile.js']
+      },
+      js: {
+        src: ['*.js']
+      },
+      test: {
+        src: ['test/**/*.js']
+      }
+    },
+    mochacli: {
+      options: {
+        reporter: 'nyan',
+        bail: true
+      },
+      all: ['test/*.js']
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      js: {
+        files: '<%= jshint.js.src %>',
+        tasks: ['jshint:js']
+      },
+      test: {
+        files: '<%= jshint.test.src %>',
+        tasks: ['jshint:test']
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['jshint', 'mochacli']);
+};
