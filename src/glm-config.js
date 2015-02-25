59ef86c177e64f3fef5a6f812b4e2f5c04ae4dfc
Julien VIDAL <jvidal@teads.tv>
2015-02-25 14:28:23 +0100

    (WIP) Code cleaning
    - Pass to promise system
    - Code must be simplified

--- src/glm-config.js
+++ src/glm-config.js
@@ -1,6 +1,9 @@
 exports.exports = {
   'github' : {
     user      : process.env.GITHUB_USER,
-    token     : process.env.GITHUB_TOKEN
+    token     : process.env.GITHUB_TOKEN,
+    init      : {
+      version: "3.0.0"
+    }
   }
 };
