4e161d0dbc8bfcf301d54fc6e9b0a660438215f1
Julien VIDAL <jvidal@teads.tv>
2015-06-08 14:27:57 +0200

    Export command added
    - Work but not stable on tricky calls like path unreachable or other potential errors

--- src/cmd/export.js
+++ src/cmd/export.js
@@ -1,10 +1,39 @@
 var GitHubApi = require("github");
 var q         = require("q");
+var fs        = require("fs");
 var config    = require("../glm-config");
 var logger    = require("../services/logger");
 
 var github    = new GitHubApi(config.github.init);
 
-var cmdExport = function cmdExport(repository, exportFile){};
+function getLabels(origin){
+  var githubGetLabels = q.nfbind(github.issues.getLabels);
+  return githubGetLabels({
+    user: config.github.user,
+    repo: origin
+  });
+}
+
+function writeLabels(labels, path){
+  labels = labels.map(function(githubLabel){
+    return {
+      name : githubLabel.name,
+      color : githubLabel.color
+    };
+  });
+  fs.writeFileSync(path, JSON.stringify(labels, null, 4), 'utf8');
+}
+
+var cmdExport = function cmdExport(repository, exportFile){
+  github.authenticate({
+    type: "oauth",
+    token: config.github.token
+  });
+  getLabels(repository)
+    .then(function(labels){
+      writeLabels(labels, exportFile);
+    })
+    .catch(console.error);
+};
 
 module.exports = cmdExport;
